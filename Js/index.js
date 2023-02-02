b3116f69211057b7d82844af871cd8437f441d2d
Julio <juliocesarap.df@gmail.com>
2023-02-01 23:14:27 -0300

    Fazendo um array que contem imagem dos jogos

---
+++ Js/index.js
@@ -0,0 +1,11 @@
+var ArrayGame = 
+[
+"https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png",
+"https://upload.wikimedia.org/wikipedia/pt/0/06/TW3_Wild_Hunt.png",
+"https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/pt_BR/games/switch/s/stardew-valley-switch/hero"
+]
+
+for(var i = 0; i < 3;i++){
+    document.write('<img src=' + ArrayGame[i] + '>');
+}
+
