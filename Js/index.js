0712874fe04aef5e60fb392008e37f5efe268e4b
Julio <juliocesarap.df@gmail.com>
2023-02-04 23:04:03 -0300

    colocando função de adicionar imagens

--- Js/index.js
+++ Js/index.js
@@ -1,29 +1,9 @@
-var ArrayGame =
-    [
-        "https://image.api.playstation.com/vulcan/ap/rnd/202207/1210/4xJ8XB3bi888QTLZYdl7Oi0s.png",
-        "https://upload.wikimedia.org/wikipedia/pt/0/06/TW3_Wild_Hunt.png",
-        "https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/pt_BR/games/switch/s/stardew-valley-switch/hero"
-    ]
+function adicionarJogo(){
+    var jogosFavoritos = document.getElementById('filme').value;
+    var ListarJogos = document.getElementById('listaJogos');
 
-    
 
-function ADDImagem(){
+    ListarJogos.innerHTML = ListarJogos.innerHTML + '<img src=' + jogosFavoritos + '>';
 
-    for(var i = 0; i < 3; i++){
-
-        if (i == 0) {
-            document.write('<img src=' + ArrayGame[i] + '>');
-        }
-        else if (i == 1) {
-            document.write('<img src=' + ArrayGame[i] + '>');
-        }
-        else if (i == 2) {
-            document.write('<img src=' + ArrayGame[i] + '>');
-        }
-        else {
-            alert("Essa imagem já existe");
-        }
-    }
+    document.getElementById('filme').value = "";
 }
-
-document.write('<input type="button" onclick="ADDImagem()" value="Adicionar Imagem">');
